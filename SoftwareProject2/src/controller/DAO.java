450fd529143a0a3813e03323a8d3c276c8535b76
max <max@192.168.111.201>
2016-10-29 16:07:46 +0200

    library mysql connector gelinkt, poging tot connectie in main acces denied :/

---
+++ SoftwareProject2/src/controller/DAO.java
@@ -0,0 +1,8 @@
+package controller;
+
+import java.sql.*;
+
+public abstract class DAO {
+
+	
+}
--- SoftwareProject2/src/controller/Main.java
+++ SoftwareProject2/src/controller/Main.java
@@ -1,7 +1,30 @@
 package controller;
+import java.sql.*;
+
+
 import controller.Login;
 
 public class Main {
 	public static void main(String[] args) {
+	// connectie maken
+		try
+		{
+		Connection con = DriverManager.getConnection("jdbc:mysql://dt5.ehb.be/phpmyadmin/SP2GR1","SP2GR1","6xBfsv");
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery("select * from Login");
+		while (rs.next())
+		{
+		Integer id = rs.getInt("LoginID");
+		String naam = rs.getString("Usernaam");
+		String ww = rs.getString("Wachtwoord");
+		System.out.println(id + "" + "naam: " + naam + "  wachtwoord: "+ ww);
+		}
+		
+		}
+		catch (SQLException exc)
+		{
+			System.out.println("t'heeft ni gewerkt");
+			System.out.println(exc.getMessage());
+		}
 	}
 }
