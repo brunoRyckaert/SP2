c08379cf56972022ec5cfa4cd6cbb7908db30acd
ilias <ilias@10.3.101.14>
2016-10-30 20:15:38 +0100

    connectie voor superklasse gemaakt en dao_personeel create gemaakt

--- SoftwareProject2/src/model/DAO_Connect.java
+++ SoftwareProject2/src/model/DAO_Connect.java
@@ -1,8 +1,7 @@
 package model;
 
-import java.sql.Connection;
+
 import java.sql.DriverManager;
-import java.sql.*;
 
 //CTRL + SHIFT + O pour générer les imports
 
@@ -11,23 +10,55 @@ import java.sql.*;
 	 *
 	 */
 	public class DAO_Connect {
-	  public static void main(String[] args) {      
-	    try {
-	      Class.forName("com.mysql.jdbc.Driver");
-	      System.out.println("Driver O.K.");
+		  public static void main(String[] args) {      
+			   try {
+			      Class.forName("com.mysql.jdbc.Driver");
+			      System.out.println("Driver O.K.");
 
-	      String url = "jdbc:mysql://https://dt5.ehb.be/phpmyadmin";
-	      String user = "SP2GR1";
-	      String passwd = "6xBfsv";
+			    //  String url = "jdbc:mysql://dt5.ehb.be:443/phpmyadmin";
+			   
 
-	      Connection conn = DriverManager.getConnection(url, user, passwd);
-	      System.out.println("Connexion effective !");         
-	         
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	    }      
-	  }
+			      DriverManager.getConnection("jdbc:mysql://dt5.ehb.be/SP2GR1","SP2GR1","6xBfsv");
+			      System.out.println("Connexion effective !");         
+			         System.out.println("conn");
+			    } catch (Exception e) {
+			      e.printStackTrace();
+			    }      
+			  }
 	}
+	
+		/*	  try
+				{
+					if( con == null || con.isClosed())
+					{
+						con = DriverManager.getConnection("jdbc:mysql://dt5.ehb.be/SP2GR1","SP2GR1","6xBfsv");
+					}
+					if( con == null || con.isClosed())
+					{
+						return null;
+					}
+				PreparedStatement stmt = con.prepareStatement("u statement");
+				ResultSet rs = stmt.executeQuery();
+				Login log = new Login();
+				while (rs.next())
+				{
+				/* 
+				log.setId(rs.getInt("LoginID"));
+				log.setUsername(rs.getString("Usernaam"));
+				log.setWachtwoord(rs.getString("Wachtwoord"));
+				
+				}
+				con.close();
+				return log;
+				}
+				catch (SQLException exc)
+				{
+					System.out.println("PROBLEEM: "+exc.getMessage());
+					System.out.println("fout code: "+ exc.getErrorCode());
+					con.close();
+					return null;
+				}
+	*/
 
 
 
--- SoftwareProject2/src/model/DAO_Personeel.java
+++ SoftwareProject2/src/model/DAO_Personeel.java
@@ -8,8 +8,9 @@ import com.mysql.jdbc.PreparedStatement;
 
 public class DAO_Personeel extends DAO_Connect {
 
-	public static boolean createPersoneel(int a,int b, String c) throws SQLException
+	public static boolean createPersoneel(Personeelslid a) throws SQLException
 	{
+		
 		 Connection con = null; 
 		PreparedStatement pstmt = null;  
         try {
@@ -18,16 +19,16 @@ public class DAO_Personeel extends DAO_Connect {
 
             try {
 				pstmt = (PreparedStatement) con.prepareStatement(
-				            "INSERT INTO Kassier Values (?,?,?)" );
+				            "INSERT INTO Kassier Values ("+ a.getNaam() + ", " + a.getDatabaseID()  );
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
                       
 
-            pstmt.setString(1, c);
-            pstmt.setInt(2, b);
-            pstmt.setInt(3 ,a);
+            pstmt.setString(1, a.getNaam());
+            pstmt.setInt(2, a.getDatabaseID());
+            
             pstmt.executeUpdate();
         }
         finally {
@@ -38,6 +39,68 @@ public class DAO_Personeel extends DAO_Connect {
 		return true;
 		
 	}
+	
+	public static boolean deletePersoneel(Personeelslid a) throws SQLException
+	{
+		 Connection con = null; 
+		PreparedStatement pstmt = null;  
+        try {
+            con = (Connection) DriverManager.getConnection(
+                      "jdbc:default:connection");
+
+            try {
+				pstmt = (PreparedStatement) con.prepareStatement(
+				            "DELETE FROM Kassier WHERE KassierID = " + a.getDatabaseID() + ";" );
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+                      
+
+            
+            pstmt.setInt(1, a.getDatabaseID());
+            pstmt.execute();
+        }
+        finally {
+            if (pstmt != null) pstmt.close();
+        }
+    
+		
+		return true;
+		
+	}
+	
+	/*public static void main(String[] args) {
+		Personeelslid a = null;
+		
+		try {
+			createPersoneel(a);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}*/
+	  public static void main(String[] args) {      
+		    try {
+		      Class.forName("com.mysql.jdbc.Driver");
+		      System.out.println("Driver O.K.");
+
+		      String url = "jdbc:mysql://dt5.ehb.be:443/phpmyadmin";
+		      String user = "SP2GR1";
+		      String passwd = "6xBfsv";
+		      
+
+		      DriverManager.getConnection(url, user, passwd);
+		      System.out.println("Connexion effective !");         
+		         System.out.println("conn");
+		    } catch (Exception e) {
+		      e.printStackTrace();
+		    }      
+		  }
+	
+	
 	
 	
 }
