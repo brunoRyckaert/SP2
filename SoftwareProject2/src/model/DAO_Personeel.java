67d1d89d0c587f30ffee176f557b9197a8e243cb
ilias <ilias@10.3.101.14>
2016-10-30 14:20:09 +0100

    ,

--- SoftwareProject2/src/model/DAO_Personeel.java
+++ SoftwareProject2/src/model/DAO_Personeel.java
@@ -2,5 +2,29 @@ package model;
 
 public class DAO_Personeel extends DAO_Connect {
 
-	
+	public static boolean createPersoneel(int a,int b, String c)
+	{
+		 Connection con = null; 
+		PreparedStatement pstmt = null;  
+        try {
+            con = DriverManager.getConnection(
+                      "jdbc:default:connection");
+
+            pstmt = con.prepareStatement(
+                        "INSERT INTO Kassier Values (?,?,?)" );
+                      
+
+            pstmt.setString(1, c);
+            pstmt.setInt(2, b);
+            pstmt.setInt(3 ,a);
+            pstmt.executeUpdate();
+        }
+        finally {
+            if (pstmt != null) pstmt.close();
+        }
+    
+		
+		return true;
+		
+	}
 }
