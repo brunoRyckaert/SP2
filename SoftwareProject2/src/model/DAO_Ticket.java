5af83123ede3543fd67d2a0d5640840bdf5bad81
ilias <ilias@PCIliasBenchikh>
2016-11-12 23:39:39 +0100

    ticket en dao ticket veranderd

---
+++ SoftwareProject2/src/model/DAO_Ticket.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+
+public class DAO_Ticket {
+	
+	public static void createTicket(Ticket a) throws SQLException{
+	 Connection con = null; 
+		PreparedStatement pstmt = null;  
+     try {
+       //  con = (Connection) DriverManager.getConnection(
+         //          "jdbc:default:connection");
+    	 con = (Connection) DriverManager.getConnection("jdbc:mysql://dt5.ehb.be/SP2GR1","SP2GR1","6xBfsv");
+
+         try {
+				pstmt = (PreparedStatement) con.prepareStatement(
+					"INSERT INTO `Ticket` (`TicketID`, `TicketSoortID`, `HeenDatum`, `TerugDatum`, `BeginStation`, `EindStation`, `Prijs`, `Korting`, `AantalReizigers`, `Aankooptijd`, `Klasse`) VALUES (NULL,"+ a.getTicketsoortID().getTicketSoortID()+","+a.getHeenDatum()+","+a.getTerugDatum()+","+a.getBeginStation()+","+a.getEindStation()+","+a.getPrijs()+","+ a.getKorting() +","+a.getAantalReizigers()+", CURRENT_TIMESTAMP, '1');"
+		);	
+         } catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+                   
+
+     
+         pstmt.executeUpdate();
+     }
+     finally {
+         if (pstmt != null) pstmt.close();
+     }
+ 
+		
+		
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws SQLException {
+		   try {
+			      Class.forName("com.mysql.jdbc.Driver");
+			      System.out.println("Driver O.K.");
+
+			    //  String url = "jdbc:mysql://dt5.ehb.be:443/phpmyadmin";
+			   
+
+			      DriverManager.getConnection("jdbc:mysql://dt5.ehb.be/SP2GR1","SP2GR1","6xBfsv");
+			      System.out.println("Connexion effective !");         
+			         System.out.println("conn");
+			    } catch (Exception e) {
+			      e.printStackTrace();
+			    }      
+			  
+		  
+		Date heen=new Date(2016,5,7);
+		   Date terug=new Date(2016,7,8);
+		//Ticket a=new Ticket();
+	//	createTicket(a);
+		//System.out.println("tis gemaakt");
+		   TicketSoort a=new TicketSoort();
+		   a.setTicketSoortID(1);
+		Ticket b=new Ticket(7,a,heen,terug,"Brussel-Zuid","Brussel-Noord",15,4,1,"00");
+		createTicket(b);
+		System.out.println("tis gemaakt");
+}
+}
--- SoftwareProject2/src/model/Ticket.java
+++ SoftwareProject2/src/model/Ticket.java
@@ -1,11 +1,13 @@
 package model;
 
+import java.sql.Date;
+
 public class Ticket {
 	
 	private int ticketID;
-	private int ticketsoortID;
-	private String heenDatum;
-	private int terugDatum;
+	private TicketSoort ticketsoortID;
+	private Date heenDatum;
+	private Date terugDatum;
 	private String beginStation;
 	private String eindStation;
 	private double prijs;
@@ -14,31 +16,39 @@ public class Ticket {
 	private String aankoopTijd;
 	public enum Klasse {Eerste , Tweede };
 	
+	
+	
+	public Ticket() {
+		super();
+	}
+	public Ticket(int ticketID, TicketSoort ticketsoortID, Date heenDatum, Date terugDatum, String beginStation,
+			String eindStation, double prijs, int korting, int aantalReizigers, String aankoopTijd) {
+		super();
+		this.ticketID = ticketID;
+		this.ticketsoortID = ticketsoortID;
+		this.heenDatum = heenDatum;
+		this.terugDatum = terugDatum;
+		this.beginStation = beginStation;
+		this.eindStation = eindStation;
+		this.prijs = prijs;
+		this.korting = korting;
+		this.aantalReizigers = aantalReizigers;
+		this.aankoopTijd = aankoopTijd;
+	}
 	public int getTicketID() {
 		return ticketID;
 	}
 	public void setTicketID(int ticketID) {
 		this.ticketID = ticketID;
 	}
-	public int getTicketsoortID() {
+	public TicketSoort getTicketsoortID() {
 		return ticketsoortID;
 	}
-	public void setTicketsoortID(int ticketsoortID) {
+	public void setTicketsoortID(TicketSoort ticketsoortID) {
 		this.ticketsoortID = ticketsoortID;
 	}
 	
-	public String getHeenDatum() {
-		return heenDatum;
-	}
-	public void setHeenDatum(String heenDatum) {
-		this.heenDatum = heenDatum;
-	}
-	public int getTerugDatum() {
-		return terugDatum;
-	}
-	public void setTerugDatum(int terugDatum) {
-		this.terugDatum = terugDatum;
-	}
+
 	public String getBeginStation() {
 		return beginStation;
 	}
@@ -74,6 +84,18 @@ public class Ticket {
 	}
 	public void setAankoopTijd(String aankoopTijd) {
 		this.aankoopTijd = aankoopTijd;
+	}
+	public Date getHeenDatum() {
+		return heenDatum;
+	}
+	public void setHeenDatum(Date heenDatum) {
+		this.heenDatum = heenDatum;
+	}
+	public Date getTerugDatum() {
+		return terugDatum;
+	}
+	public void setTerugDatum(Date terugDatum) {
+		this.terugDatum = terugDatum;
 	}
 	
 
