0e58709d5b97b6efd501dc9fbf36bf33162dae9c
Denis <Denis@EDEN02.ehb.local>
2016-11-14 11:54:34 +0100

    AbonnementDAO afgemaakt en Abonnement aangepast.

--- SoftwareProject2/src/controller/AbonnementDAO.java
+++ SoftwareProject2/src/controller/AbonnementDAO.java
@@ -7,9 +7,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import model.Abonnement;
+import java.util.Calendar;
 
 public class AbonnementDAO extends DAO {
-	/* Abonnement tabel moet nog gecreëerd worden
 	public Abonnement getAbonnement(int abonnementID)
 	{
 		try
@@ -22,8 +22,41 @@ public class AbonnementDAO extends DAO {
 			{
 				return null;
 			}
+			PreparedStatement stmt = con.prepareStatement("select * from Abonnement where abonnementId = ?");
+			stmt.setInt(1, abonnementID);
+			ResultSet rs = stmt.executeQuery();
+			Abonnement abo = new Abonnement();
+			abo.setAbonnementNummer(0);
 			
+			while (rs.next()){
+				abo.setAbonnementNummer(rs.getInt("abonnementId"));
+				abo.setKlantNummer(rs.getInt("klantId"));
+				// Stations
+				
+				Calendar calendar = Calendar.getInstance(); //Convert Timestamp naar Calendar
+				calendar.setTime(rs.getTimestamp("aankoopTijd")); // calendar zetten op aankooptijd
+				abo.setAankoopTijd(calendar);
+				calendar.setTime(rs.getTimestamp("beginDatum")); // calendar zetten op beginDatum
+				abo.setBeginDatum(calendar);
+				calendar.setTime(rs.getTimestamp("eindDatum")); // calendar zetten op eindDatum
+				abo.setEindDatum(calendar);
+				
+				abo.setGeldig(rs.getBoolean("isGeldig"));
+				abo.setPrijs(rs.getDouble("prijs"));
+				abo.setKortingNummer(rs.getInt("kortingId"));
+			}
+			con.close();
+			if(abo.getAbonnementNummer() == 0)
+			{
+				return null;
+			}
+			else{
+				return abo;
+			}
+		}
+		catch (SQLException exc)
+		{
+			return null;
 		}
 	}
-	*/
 }
--- SoftwareProject2/src/model/Abonnement.java
+++ SoftwareProject2/src/model/Abonnement.java
@@ -14,65 +14,79 @@ public class Abonnement {
 	private double prijs;
 	private int kortingNummer;
 	
-	public Abonnement(int klantNummer/*, Station beginStation, Station eindStation*/, Calendar aankoopTijd,
-			Calendar beginDatum, Calendar eindDatum, boolean isGeldig, double prijs, int kortingNummer) {
-		super();
-		// set uniek abonnementNummer
-		this.klantNummer = klantNummer;
-		//this.beginStation = beginStation;
-		//this.eindStation = eindStation;
-		this.aankoopTijd = aankoopTijd;
-		this.beginDatum = beginDatum;
-		this.eindDatum = eindDatum;
-		this.isGeldig = true;
-		this.prijs = prijs;
-		this.kortingNummer = kortingNummer;
-	}
+	public Abonnement()
+	{
+		
+	}	
 	
 	// getter/setter begin-en eindStation
-	public int getAbonnementNummer() {
-		return abonnementNummer;
-	}
-	public int getKlantNummer() {
-		return klantNummer;
-	}
-	public Calendar getAankoopTijd() {
-		return aankoopTijd;
-	}
+	
 	public Calendar getBeginDatum() {
 		return beginDatum;
 	}
-	public Calendar getEindDatum() {
-		return eindDatum;
-	}
-	public boolean isGeldig() {
-		return isGeldig;
-	}
-	public double getPrijs() {
-		return prijs;
-	}
-	public int getKortingNummer() {
-		return kortingNummer;
-	}
-	public void setKlantNummer(int klantNummer) {
-		this.klantNummer = klantNummer;
-	}
+	
 	public void setBeginDatum(Calendar beginDatum) {
 		this.beginDatum = beginDatum;
 		// eindDatum is altijd 1 jaar later
 		this.eindDatum = beginDatum;
 		this.eindDatum.add(eindDatum.YEAR, +1);
 	}
+	
+	public int getAbonnementNummer() {
+		return abonnementNummer;
+	}
+
+	public void setAbonnementNummer(int abonnementNummer) {
+		this.abonnementNummer = abonnementNummer;
+	}
+
+	public int getKlantNummer() {
+		return klantNummer;
+	}
+
+	public void setKlantNummer(int klantNummer) {
+		this.klantNummer = klantNummer;
+	}
+
+	public Calendar getAankoopTijd() {
+		return aankoopTijd;
+	}
+
+	public void setAankoopTijd(Calendar aankoopTijd) {
+		this.aankoopTijd = aankoopTijd;
+	}
+
+	public boolean isGeldig() {
+		return isGeldig;
+	}
+
+	public void setGeldig(boolean isGeldig) {
+		this.isGeldig = isGeldig;
+	}
+
+	public double getPrijs() {
+		return prijs;
+	}
+
+	public void setPrijs(double prijs) {
+		this.prijs = prijs;
+	}
+
+	public int getKortingNummer() {
+		return kortingNummer;
+	}
+
+	public void setKortingNummer(int kortingNummer) {
+		this.kortingNummer = kortingNummer;
+	}
+
+	public Calendar getEindDatum() {
+		return eindDatum;
+	}
 	public void setEindDatum(Calendar eindDatum) {
 		this.eindDatum = eindDatum;
 		// beginDatum is altijd 1 jaar vroeger
 		this.beginDatum = eindDatum;
 		this.beginDatum.add(beginDatum.YEAR, -1);
-	}
-	public void setGeldig(boolean isGeldig) {
-		this.isGeldig = isGeldig;
-	}
-	public void setKortingNummer(int kortingNummer) {
-		this.kortingNummer = kortingNummer;
-	}
+	}	
 }
