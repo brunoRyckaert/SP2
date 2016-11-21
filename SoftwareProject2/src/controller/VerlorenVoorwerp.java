7fbc10227316035a718d7aa1728423656088cbc8
ilias <ilias@10.3.210.85>
2016-11-21 10:38:17 +0100

    verlorenvoorwerp = klasse en dao

---
+++ SoftwareProject2/src/controller/VerlorenVoorwerp.java
@@ -0,0 +1,53 @@
+package controller;
+
+public class VerlorenVoorwerp {
+
+	private int itemId;
+	private int klantId;
+	private String beschrijving;
+	private String station;
+	
+	
+	
+	public VerlorenVoorwerp() {
+		super();
+	}
+	
+	
+	public VerlorenVoorwerp(int itemId, int klantId, String beschrijving, String station) {
+		super();
+		this.itemId = itemId;
+		this.klantId = klantId;
+		this.beschrijving = beschrijving;
+		this.station = station;
+	}
+
+
+	public int getItemId() {
+		return itemId;
+	}
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+	public int getKlantId() {
+		return klantId;
+	}
+	public void setKlantId(int klantId) {
+		this.klantId = klantId;
+	}
+	public String getBeschrijving() {
+		return beschrijving;
+	}
+	public void setBeschrijving(String beschrijving) {
+		this.beschrijving = beschrijving;
+	}
+	public String getStation() {
+		return station;
+	}
+	public void setStation(String station) {
+		this.station = station;
+	}
+	
+	
+	
+}
