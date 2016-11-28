8d3d6e4a994332853ab503e3214b387f920eabb0
ilias <ilias@PCIliasBenchikh>
2016-11-28 12:27:55 +0100

    lijst gemaakt in verlorenvoorwerpdao

--- SoftwareProject2/src/controller/VerlorenVoorwerpDAo.java
+++ SoftwareProject2/src/controller/VerlorenVoorwerpDAo.java
@@ -4,6 +4,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import controller.DAO;
 import model.Personeel;
@@ -69,7 +70,7 @@ public class VerlorenVoorwerpDAo extends DAO {
 				return null;
 			}
 
-		PreparedStatement stmt = con.prepareStatement("SELECT * FROM `VerlorenItem` WHERE `Beschrijving` LIKE ?");
+		PreparedStatement stmt = con.prepareStatement("SELECT * FROM `VerlorenItem` WHERE Beschrijving like '%' ? '%'");
 		stmt.setString(1,be);
 		ResultSet rs = stmt.executeQuery();
 		VerlorenVoorwerp pers = new VerlorenVoorwerp();
@@ -102,9 +103,111 @@ public class VerlorenVoorwerpDAo extends DAO {
 			return null;
 		}
 	}
+	
+	public static ArrayList<VerlorenVoorwerp> selectLijstVVByNaam(String be) throws SQLException{
+		try
+		{
+			if( con == null || con.isClosed())
+			{
+				con = DAO.getInstance();
+			}
+			if( con == null || con.isClosed())
+			{
+				return null;
+			}
 
+		PreparedStatement stmt = con.prepareStatement("SELECT * FROM `VerlorenItem` WHERE Beschrijving like '%' ? '%'");
+		stmt.setString(1,be);
+		ResultSet rs = stmt.executeQuery();
+		ArrayList<VerlorenVoorwerp> lijst = new ArrayList<VerlorenVoorwerp>();
+		
+		//pers.setKlantId(null);
+		//int adresID = 0;
+
+		while (rs.next())
+		{
+//		pers.setId(rs.getInt("KassierID"));
+//		pers.setNaam(rs.getString("Naam"));
+//		pers.setGeboorteDatum(rs.getDate("geboortedatum"));
+//		pers.setIsActief(rs.getBoolean("actief"));
+//		adresID = rs.getInt("adresID");
+//		pers.setAdmin(rs.getBoolean("isAdmin"));
+			VerlorenVoorwerp pers = new VerlorenVoorwerp();
+			pers.setItemId(rs.getInt("ItemID"));
+			pers.setKlantId(rs.getInt("KlantID"));
+			pers.setBeschrijving(rs.getString("Beschrijving"));
+			pers.setStation(rs.getString("Station"));
+			
+			lijst.add(pers);
+			//pers = null;
+		}
+		
+		con.close();
+		return lijst;
+		//System.out.println(pers.toString());
+		}
+		catch (SQLException exc)
+		{
+			System.out.println("PROBLEEM: "+exc.getMessage());
+			System.out.println("fout code: "+ exc.getErrorCode());
+			return null;
+			
+		}
+	}
+	public static void updateVerlorenvoorwerp (int a, int b ) throws SQLException {
+		
+		try {
+			if (con == null || con.isClosed()) {
+				con = DAO.getInstance();
+			}
+			if (con == null || con.isClosed()) {
+
+				
+			}
+			PreparedStatement stmt = con.prepareStatement(""
+					+ "UPDATE `VerlorenItem` SET `KlantID` = ? WHERE `VerlorenItem`.`ItemID` = ?;");
+			
+			stmt.setInt(1,a);
+			stmt.setInt(2,b);
+			
+			/*stmt.setInt(1, t.getTicketSoortID());
+			stmt.setDate(2, (Date) t.getHeenDatum());
+			stmt.setDate(3, (Date) t.getTerugDatum());
+			stmt.setString(4, t.getBeginStation());
+			stmt.setString(5, t.getEindStation());
+			stmt.setDouble(6, t.getPrijs());
+			stmt.setFloat(7, t.getKorting());
+			stmt.setInt(8, t.getAantalReizigers());
+			stmt.setInt(9, t.getKlasseGetal());*/
+			int updateCount = stmt.executeUpdate();
+
+			
+			con.close();
+	
+	
+		} catch (SQLException exc) {
+			System.out.println("PROBLEEM: " + exc.getMessage());
+			System.out.println("fout code: " + exc.getErrorCode());
+			con.close();
+		}
+		finally
+		{
+			con.close();
+		}
+		
+	}
+
+	
 	public static void main(String[] args) throws SQLException {
-		System.out.println(selectVVByNaam("k3 map").toString());
+		//System.out.println(selectVVByNaam("k3").toString());
+		ArrayList<VerlorenVoorwerp> lijst= selectLijstVVByNaam("k");
+		for (int i = 0; i < lijst.size(); i++) {
+			System.out.println(lijst.get(i).toString());
+			
+			
+		}
+		
+		updateVerlorenvoorwerp (8, 15 );
 		//p
 	}
 }
