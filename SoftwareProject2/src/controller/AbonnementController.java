444a731edc77a19c553ab1919fa0471e134dfed5
ilias12345 <ilias-benchikh@hotmail.com>
2017-01-02 23:49:42 +0100

    Add files via upload

--- SoftwareProject2/src/controller/AbonnementController.java
+++ SoftwareProject2/src/controller/AbonnementController.java
@@ -15,7 +15,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -23,10 +23,15 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import model.Abonnement;
 import model.Definitions;
+import model.Klant;
+import model.Settings;
+import model.Station;
+import model.Strings;
 
 public class AbonnementController implements Initializable {
 
-	 ArrayList<String>stations = Definitions.getStations();
+	 ArrayList<Station> stations = Definitions.getStations();
+	 ArrayList<String> klantennr = new ArrayList<String>();
 	@FXML
 	private Label lblklantid; 
 	@FXML
@@ -64,40 +69,128 @@ public class AbonnementController implements Initializable {
 	private Button btnBetalen;
 	@FXML
 	private ComboBox<String> cbklasse ;
+	@FXML
+	private Label lblError=new Label();
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		ObservableList<String> klasse = FXCollections.observableArrayList("1ste klasse","2 de klasse");
+		
+		this.lblbeginDatum.setText(Strings.lblbeginDatum[Settings.getInstance().getTaal().getValue()]);
+		this.lblBeginstation.setText(Strings.vanStation[Settings.getInstance().getTaal().getValue()]);
+		this.lbleindDatum.setText(Strings.lblEindDatum[Settings.getInstance().getTaal().getValue()]);
+		this.lblEindStation.setText(Strings.naarStation[Settings.getInstance().getTaal().getValue()]);
+		this.lblklantid.setText(Strings.klantId[Settings.getInstance().getTaal().getValue()]);
+		this.lblKlasse.setText(Strings.lblklasse[Settings.getInstance().getTaal().getValue()]);
+		this.lblkorting.setText(Strings.korting[Settings.getInstance().getTaal().getValue()]);
+		this.lblPrijs.setText(Strings.lblprijs[Settings.getInstance().getTaal().getValue()]);
+		
+		
+		
+		
+		ObservableList<String> klasse = FXCollections.observableArrayList(Strings.eersteKlasse[Settings.getInstance().getTaal().getValue()],Strings.tweedeKlasse[Settings.getInstance().getTaal().getValue()]);
+	
 				cbklasse.setItems(klasse);
-				TextFields.bindAutoCompletion(this.TFBeginStation,  stations);
-				TextFields.bindAutoCompletion(this.TFEindStation,  stations);
+				ArrayList<String> stationsnamen = new ArrayList<String>();
+				for (int i = 0; i < stations.size(); i++) {
+				
+					stationsnamen.add(stations.get(i).getNaam().get(0));
+				}
+				KlantDAO k = new KlantDAO();
+				ArrayList<Klant> a = k.alleKlanten();
+				for (int i = 0; i < a.size(); i++) {
+					klantennr.add(String.valueOf(a.get(i).getNaam()));
+				}
+				
+				TextFields.bindAutoCompletion(this.TFBeginStation,  stationsnamen);
+				TextFields.bindAutoCompletion(this.TFEindStation,  stationsnamen);
+				TextFields.bindAutoCompletion(this.TFKlantid, this.klantennr);
+				lblError.setText("");
 	}
 	
 	public void betalen(ActionEvent event)
 	{
-		/*private int abonnementNummer;
-	private int klantNummer;
-	private String beginStation;
-	
-	private String eindStation;
-	private Timestamp aankoopTijd;
-	private Date beginDatum;
-	private Date eindDatum;
-	private boolean isGeldig;
-	private double prijs;
-	private int kortingNummer;*/
-		Abonnement b = new Abonnement();
-		b.setKlantNummer(Integer.parseInt(this.TFKlantid.getText()));
-		b.setBeginStation(this.TFBeginStation.getText());
-		b.setEindStation(this.TFEindStation.getText());
-		b.setBeginDatum(Date.valueOf(this.DTBeginDatum.toString()));
-		b.setEindDatum(Date.valueOf(this.DTEindDatum.toString()));
-		b.setPrijs(Double.parseDouble(this.TFPrijs.getText()));
-		AbonnementDAO a = new AbonnementDAO();
-		a.insertAbonnement(b);
-		area.setText("Abonnement is aangemaakt");
-		clearalleVelden();
+		
+		ArrayList<String> stationsnamen = new ArrayList<String>();
+		for (int i = 0; i < stations.size(); i++) {
+		
+			stationsnamen.add(stations.get(i).getNaam().toString());
+		}
+		KlantDAO k = new KlantDAO();
+		ArrayList<Klant> a = k.alleKlanten();
+		for (int i = 0; i < a.size(); i++) {
+			klantennr.add(String.valueOf(a.get(i).getNaam()));
+		}
+		System.out.println(TFKlantid.getText());
+		Klant klant = a.get(klantennr.indexOf(TFKlantid.getText()));
+//		System.out.println(TFBeginStation.getText());
+//		System.out.println(this.TFEindStation.getText());
+//		System.out.println(this.TFKlantid.getText());
+//		System.out.println(this.DTEindDatum.getValue());
+		
+//		System.out.println(stationsnamen.contains(TFBeginStation.getText()));
+//		System.out.println( stationsnamen.contains(this.TFEindStation.getText()));
+//		System.out.println(this.TFBeginStation.getText().equals(TFEindStation.getText()));
+		System.out.println(Definitions.getStationsNamen().contains(TFBeginStation.getText()));
+		System.out.println(Definitions.getStationsNamen().contains(TFEindStation.getText()));
+		//Definitions.getStationsNamen().contains(TFBeginStation.getText()) && Definitions.getStationsNamen().contains(TFEindStation.getText())
+		if( 
+//				&& stationsnamen.contains(this.TFEindStation.getText())
+//				&& (this.TFBeginStation.getText().equals(TFEindStation.getText()) == false)
+//				&& klantennr.contains(this.TFKlantid.getText()) 
+				 Definitions.getStationsNamen().contains(TFBeginStation.getText())
+				&& Definitions.getStationsNamen().contains(TFEindStation.getText())
+				
+				&& this.DTBeginDatum.getValue().isBefore(this.DTEindDatum.getValue()))
+		{
+			Abonnement b = new Abonnement();
+			//	b.setKlantNummer(Integer.parseInt(this.TFKlantid.getText()));
+				b.setBeginStation(this.TFBeginStation.getText());
+				b.setEindStation(this.TFEindStation.getText());
+				b.setBeginDatum(Date.valueOf(this.DTBeginDatum.getValue()));
+				b.setEindDatum(Date.valueOf(this.DTEindDatum.getValue()));
+				//b.setPrijs(Double.parseDouble(this.TFPrijs.getText()));
+				b.setKlantId(klant.getKlantID());
+				b.setKortingId(Integer.parseInt(this.TFKorting.getText()));
+				
+				System.out.println("je passe dans le if");
+				
+				
+				
+				AbonnementDAO abdao = new AbonnementDAO();
+				abdao.add(b);
+				area.setText(Strings.Abmake[Settings.getInstance().getTaal().getValue()]);
+				clearalleVelden();
+			/*Abonnement b = new Abonnement();
+			//	b.setKlantNummer(Integer.parseInt(this.TFKlantid.getText()));
+				b.setBeginStation(this.TFBeginStation.getText());
+				b.setEindStation(this.TFEindStation.getText());
+				b.setBeginDatum(Date.valueOf(this.DTBeginDatum.toString()));
+				b.setEindDatum(Date.valueOf(this.DTEindDatum.toString()));
+				b.setPrijs(Double.parseDouble(this.TFPrijs.getText()));
+				b.setKlantId(Integer.parseInt(this.TFKlantid.getText()));
+				b.setKortingId(Integer.parseInt(this.TFKorting.getText()));
+				System.out.println("je passe dans le if");
+				b.setGeldig(true);
+				
+				
+				AbonnementDAO abdao = new AbonnementDAO();
+				abdao.add(b);
+				area.setText(Strings.Abmake[Settings.getInstance().getTaal().getValue()]);
+				clearalleVelden();*/
+		
+		
+		}
+		
+		else
+		{
+		/*	this.lblError.setText(Strings.lblError[Settings.getInstance().getTaal().getValue()]);
+			area.setText("fout invoer bij de velden");
+		*/	
+			this.lblError.setText(Strings.lblError[Settings.getInstance().getTaal().getValue()]);
+			area.setText(Strings.lblError[Settings.getInstance().getTaal().getValue()]);
+		}
+		
 	}
 	
 	public void clearalleVelden()
